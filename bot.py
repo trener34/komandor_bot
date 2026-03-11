0f91028c121856f2330f8c7acbfeaad8d1684c3b
trener34 <micro16trener@gmail.com>
2026-03-11 19:15:46 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,22 +1,18 @@
-import sqlite3
+import asyncio
 from aiogram import Bot, Dispatcher
-
 from config import BOT_TOKEN
 from database import init_db
-from handlers import start, admin, game
-from handlers import broadcast
-dp.include_router(broadcast.router)
+from handlers import start, admin, game, broadcast
 
 async def main():
-    init_db()
+    await init_db()
     bot = Bot(BOT_TOKEN)
     dp = Dispatcher()
     dp.include_router(start.router)
     dp.include_router(admin.router)
     dp.include_router(game.router)
+    dp.include_router(broadcast.router)
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
     asyncio.run(main())
-
-
