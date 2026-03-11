0aab0f1ea79e7f07ecefa483900012fb7be70b26
trener34 <micro16trener@gmail.com>
2026-03-11 19:43:58 +0300

    Update admin.py

--- handlers/admin.py
+++ handlers/admin.py
@@ -1,8 +1,16 @@
 from aiogram import Router
 from aiogram.types import Message
+from aiogram.filters import Command
 
 router = Router()
 
-@router.message(commands=["admin"])
-async def cmd_admin(message: Message):
-    await message.answer("Это админ-команда. Тут можно добавить управление ботом.")
+ADMIN_ID = 123456789  # сюда вставь свой Telegram ID
+
+@router.message(Command("admin"))
+async def admin_panel(message: Message):
+
+    if message.from_user.id != ADMIN_ID:
+        await message.answer("❌ Нет доступа")
+        return
+
+    await message.answer("⚙️ Админ панель открыта")
