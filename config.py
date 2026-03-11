f401a46fea3c2060c6e5e66f1b98b09dfd64e7a6
trener34 <micro16trener@gmail.com>
2026-03-11 19:28:16 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+if not BOT_TOKEN:
+    raise ValueError("BOT_TOKEN не задан. Установите переменную окружения BOT_TOKEN")
 ADMIN_ID = 184818027  # <-- твой telegram id
 MAX_PLAYERS = 12
