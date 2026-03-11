bab8ee50571f699a71a716c50518bc786676e9d1
trener34 <micro16trener@gmail.com>
2026-03-11 19:30:46 +0300

    Update game.py

--- handlers/game.py
+++ handlers/game.py
@@ -1,48 +1,16 @@
-from aiogram import Router, F
-from aiogram.types import CallbackQuery
+from aiogram import Router
+from aiogram.types import Message
 from services.game_service import GameService
-from services.list_builder import build_game_text
 
 router = Router()
+game_service = GameService()
 
+@router.message(commands=["startgame"])
+async def start_game(message: Message):
+    text = game_service.start_game(message.from_user.id)
+    await message.answer(text)
 
-@router.callback_query(F.data.startswith("go_"))
-async def go_handler(callback: CallbackQuery):
-
-    game_id = int(callback.data.split("_")[1])
-    result = await GameService.add_registration(game_id, callback.from_user.id)
-
-    if result == "closed":
-        await callback.answer("Игра уже закрыта", show_alert=True)
-        return
-
-    game = await GameService.get_active_game()
-    players = await GameService.get_full_list(game_id)
-    text = build_game_text(game[1], players)
-
-    await callback.bot.edit_message_text(
-        text,
-        chat_id=game[2],
-        message_id=game[3]
-    )
-
-    await callback.answer("Вы записаны ✅")
-
-
-@router.callback_query(F.data.startswith("no_"))
-async def no_handler(callback: CallbackQuery):
-
-    game_id = int(callback.data.split("_")[1])
-    await GameService.remove_registration(game_id, callback.from_user.id)
-
-    game = await GameService.get_active_game()
-    players = await GameService.get_full_list(game_id)
-    text = build_game_text(game[1], players)
-
-    await callback.bot.edit_message_text(
-        text,
-        chat_id=game[2],
-        message_id=game[3]
-    )
-
-    await callback.answer("Вы снялись ❌")
+@router.message(commands=["endgame"])
+async def end_game(message: Message):
+    text = game_service.end_game(message.from_user.id)
+    await message.answer(text)
